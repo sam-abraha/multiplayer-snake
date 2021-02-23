e3f17704153292aed7437fac9475e573a538a706
Lucas <Lucas@DESKTOP-A0CQ3P9.Speedport_W723_V_Typ_A_1_01_022>
2021-02-23 15:03:29 +0100

    changes

--- Snake/src/oop/java/project/Commons.java
+++ Snake/src/oop/java/project/Commons.java
@@ -2,9 +2,9 @@ package oop.java.project;
 
 
 public class Commons {
-	private static final int WIDTH=600;
-	private static final int HEIGHT=600;
-	private static final int SIZE=20;
+	public static final int WIDTH=600;
+	public static final int HEIGHT=600;
+	public static final int SIZE=20;
 	
 	
 	public static int getHeight() {
